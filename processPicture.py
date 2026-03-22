1f3c28336ae578abf802bc8c01b982edb2e968b0
Sergey <sergojaffe@gmail.com>
2026-03-22 20:24:48 +0200

    Added URI component decoding in processPicture() function

--- processPicture.py
+++ processPicture.py
@@ -1,15 +1,16 @@
 import requests
 import os
+from urllib import parse
+
 
 def processPicture(picURL: str) -> str:
     # TODO: return a list of indices to check.
     # download picture
     
     msg: str
+    picURL = parse.unquote(picURL) # decoding the URL supplied in a GET request
 
-    picURL = picURL.strip("\'\"")
-
-    print(f"\n\n\t{picURL=}\n\n\n")
+    print(f"\n\n{picURL=}\n\n")
     
     response = requests.get(picURL)
     
@@ -18,7 +19,6 @@ def processPicture(picURL: str) -> str:
         msg = f"An error occured downloading the picture. Response code: {response.status_code}"
         print(msg)
         return msg
-    
 
     # make a folder if it's not there
     if not os.path.exists("./downloads"):
@@ -29,9 +29,10 @@ def processPicture(picURL: str) -> str:
         picFile.write(response.content)
         msg = "Picture downloaded successfully!"
     
+    # TODO: find the indices for squares to tick
     print(msg)
 
+    # TODO: feed the picture into LLM and get the LLM response about what to tick
+    
+    # TODO: return a JSON-formatted response
     return msg
-    # TODO: feed the picture into LLM
-    # get the LLM response about what to tick
-    # return a list of what to tick
