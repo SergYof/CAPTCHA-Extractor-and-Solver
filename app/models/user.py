6b9f2cb005be714fb3e88b92c9bfd50d006d4ded
Sergey <sergojaffe@gmail.com>
2026-06-04 19:53:29 +0300

    Added document_filename column and moved init_db.py script in the project root directory.

--- app/models/user.py
+++ app/models/user.py
@@ -4,8 +4,10 @@ from app.extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    username = db.Column(db.String(64), unique=True)
-    password_hash = db.Column(db.String(256))
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    document_filename = db.Column(db.String(69))
 
     status = db.Column(
         db.String(20),
