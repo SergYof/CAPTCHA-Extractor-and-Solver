79031bb97546a3981a4ab8e4e09d5aefb4e685d8
Sergey <sergojaffe@gmail.com>
2026-06-06 20:15:23 +0300

    Improved readability of the extension code; started implementing the response reaction

--- extension/background.js
+++ extension/background.js
@@ -1,28 +1,62 @@
+/* This is the background script of the extension. It runs in the backend of the extension and exchanges
+information with content script (content.js) which runs in the context of a loaded web page. 
+The information is transferred via Chrome runtime, which includes functions such as 
+onMessage.addListener() for listening, and sendResponse() callback.
+
+This script accepts a message with the CAPTCHA challenge data, and queries a local API (Flask),
+bypassing PNA (Private Network Access) restrictions. */
+
+
 async function handleSubmit(msg, sendResponse) {
   console.log("Picture submit message received.");
   try {
-    // make sure the request is full
-    console.assert(msg.instructionText && msg.picURL && msg.isSpecial3x3 != null, "One or more parameters absent in message JSON");
-    
+
+    // make sure the request is full before proceeding
+    if (!(msg.instructionText && msg.picURL && msg.isSpecial3x3 != null))
+      throw new Error(
+        "One or more parameters absent in message JSON.",
+        {
+          cause: {
+            response: null,
+            error: "Malformed request: one or more of the fields is absent.",
+          },
+        }
+      );
+
+
     // encode the picture URL so there are no symbols like &=/?: since they would break the outer URL
+    // instruction text should be encoded too, because spaces break the URL as well
     const encoded_url = encodeURIComponent(msg.picURL); 
     const encoded_instructions = encodeURIComponent(msg.instructionText);
 
-    console.log("Fetching response...");
+    console.log("Fetching response from local API...");
     const response = await fetch(
       "https://127.0.0.1:5000/submit_picture" +
       `?instructionText=${encoded_instructions}` +
       `&picURL=${encoded_url}` +
       `&isSpecial3x3=${msg.isSpecial3x3}`
     );
-    
-    console.log("Sending response...");
-    sendResponse(await response.text()); // for now, response.text() works out. Better to make a JSON response later.
+
+    // make sure the fetch is successful (200-299 status codes)
+    if (!response.ok)
+      throw new Error(
+        "HTTP Error",
+        {
+          cause: await response.json(),
+        }
+      );
+
+
+    console.log("Sending response to the content script...");
+    sendResponse(await response.json());
+    console.log("Response successfuly sent to the content script.")
+
   } catch(e) {
     console.error(e.toString());
-    sendResponse({error: "Extension backend error!"})
+    sendResponse(e.cause);  // response is sent anyway
   }
 }
+
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log("Received message", msg);
--- extension/content.js
+++ extension/content.js
@@ -1,54 +1,74 @@
- /* 
-This script is intended to be run in the inner context (the CAPTCHA iframe).
-It recognizes the type of CAPTCHA on the page (dynamic / static tiles), 
-and then gets the list of all the images in the iframe, and extracts the needed ones.
-*/
+ /* This script is intended to be run in the inner context (the CAPTCHA iframe).
+It recognizes the type of CAPTCHA on the page (dynamic / static tiles), then
+gets the list of all the images in the iframe, and extracts the needed ones. */
 
-function extractImage() {    
-    // פונקציית עזר להורדת קבצים למחשב
-    const sendToServer = async (instructionText, url, isSpecial3x3) => { 
-        console.log(`CAPTCHA image URL: ${url}`);
 
-        // Send request to extension backend
-        chrome.runtime.sendMessage(
-          // message body
-          {
+async function parseResponse(response) {
+    console.log("Response from server:", response);
+
+    if (!("response" in response && "error" in response)) {
+        console.error("Malformed response - not all fields are present!");
+        return;
+    }
+
+    
+    let error = [];
+
+    if (response.response == null)
+        error = ["No response field present in response JSON"]
+    
+    if(response.error != null)
+        error = ["Error in response:", response.error]
+
+    if (error != []) {
+        console.error(...error);
+        return;
+    }
+
+    // TODO: a function which clicks the tiles with indices provided in the response
+}
+
+
+async function sendToServer(instructionText, url, isSpecial3x3) {
+    // Helper function for sending challenge information to the extension backend
+    chrome.runtime.sendMessage(
+        // message body
+        {
             type: "SUBMIT_PICTURE",
             instructionText: instructionText,
             picURL: url,
             isSpecial3x3: isSpecial3x3,
-          },
-          // callback
-          (response) => {
-            console.log("Server says:", response);
-          }
-        );
-        console.log("URL sent to the extension backend!");
-    };
+        },
+        parseResponse // callback function
+    );
+};
 
+
+function extractImage() {
     // start execution
     console.log("Starting CAPTCHA Image Extractor...");
 
     // extract CAPTCHA container div and instruction text
     const descriptionsContainer = document.querySelector('div.rc-imageselect-desc, div.rc-imageselect-desc-no-canonical');
-    const instructionText = descriptionsContainer ? descriptionsContainer.innerText.replace(/\s+/g, '_') : "captcha_image";
+    const instructionText = descriptionsContainer
+        ? descriptionsContainer.innerText.replace(/\s+/g, " ") // replace any amount of whitespaces with one space
+        : "captcha_image";
     
-    // extract CAPTCHA main image url 
+    // extract CAPTCHA main image url
     const mainImageUrl = document.getElementsByTagName('img')[0].src;
-    
-    console.log("Main Challenge Image Found. Checking for the challenge type...");
-    console.log(`Instruction text detected: ${instructionText}`); // TODO: send it together with the URL
     
     // fading tiles CAPTCHA identification
     const isSpecial3x3 = descriptionsContainer && descriptionsContainer.childNodes.length === 3;
-
+    
+    console.log(`Instruction text detected: ${instructionText}`);
+    console.log(`Main challenge image found. URL: ${mainImageUrl}`);
     console.log( isSpecial3x3 ? 
         "Detected: Special 3x3 with fading tiles." : 
         "Detected: Static grid (No fading expected)."
     );
     
-    console.log("Sending the main image URL to the server...");
     sendToServer(instructionText, mainImageUrl, isSpecial3x3);
+    console.log("Challenge info sent to the extension backend!");
 
     if (isSpecial3x3) {
         console.log("Monitoring dynamic grid for changes..."); 
@@ -94,5 +114,10 @@ function extractImage() {
             obs.disconnect(); // The MutationObserver works only one time
             extractImage(); // TODO: (if the previous line is removed) in case of fading tiles, this MutationObserver may trigger when it shouldn't
         }
-    }).observe(document.getRootNode(), {childList: true, subtree: true}); // monitor the entire document because why not
+    }).observe(
+        document.getRootNode(), // monitor the entire document because why not
+        {   
+            childList: true,
+            subtree: true
+        });
 }) ();
