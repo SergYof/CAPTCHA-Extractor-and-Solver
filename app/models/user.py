d84f75fef5d4d978782c0a2e826ac94a0ce46172
Sergey <sergojaffe@gmail.com>
2026-06-06 17:01:21 +0300

    Added an email column in the database for contacting the users

--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
 
     document_filename = db.Column(db.String(64))
