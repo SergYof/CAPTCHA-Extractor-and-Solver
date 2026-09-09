7e34c9d8d8b03df4f08cf61de237d532b80c5ecd
Sergey <sergojaffe@gmail.com>
2026-09-09 16:35:45 +0300

    Add pretty comment about not wanting to break terms of service in the function

--- socketServer/queryAPI.py
+++ socketServer/queryAPI.py
@@ -3,5 +3,9 @@ from requests import post
 
 
 def queryAPI():
-    # TODO which sadly won't be done...
+    """ This is the place I'd put the call to the computer vision API
+    and get the CAPTCHA solved. However, automating solving CAPTCHAs
+    breaks multiple Terms of Service and, in fact, is a security threat.
+    I will not do that, but rather consider the project done at this
+    point, since I've demonstrated my abilities and learned a lot. """
     return
